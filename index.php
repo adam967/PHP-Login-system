6fa3e1bf30c08ba6b55b9047e7fe69d6b871c498
adam967 <adam996677@gmail.com>
2018-12-19 17:24:26 +0100

    added login form

--- index.php
+++ index.php
@@ -1,1 +1,54 @@
-<?php echo "<p> Hello World </p>"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="robots" content="follow">
+	
+    <title>Title</title>
+	<base href="/">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/css/uikit.min.css" />
+  </head>
+	
+  <body>
+	<div class="uk-section uk-container uk-text-center">
+		<div class="uk.grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid=" ">
+						<form class="uk-form-stacked js-login">
+
+				<div class="uk-margin">
+					<label class="uk-form-label" for="form-stacked-text">Email</label>
+					<div class="uk-form-controls">
+						<input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="Your Email">
+					</div>
+				</div>
+
+				<div class="uk-margin">
+				
+								<div class="uk-margin">
+					<label class="uk-form-label" for="form-stacked-text">Password</label>
+					<div class="uk-form-controls">
+						<input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+					</div>
+				</div>
+				
+				<div class="uk.margin">
+					<button class="uk-button uk-button-default" type="submit">LOGIN</button>
+				</div>
+			
+
+
+</form>
+		</div>
+	</div>
+  
+  
+  
+  
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit-icons.min.js"></script>
+  </body>
+</html>
